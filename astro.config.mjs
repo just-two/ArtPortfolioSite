b7417f7951b7b35f4e6c9c031513c891cc0485ed
wrb95 <w.r.brennan.95@gmail.com>
2026-09-15 19:59:56 -0400

    use the TL domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://just-two.github.io',
-  base: '/ArtPortfolioSite', // Exact casing of your GitHub repo name
+  site: 'https://tahsinloqman.com',
 });
